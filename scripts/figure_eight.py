0718af7a29c41952b7e64016e9f95b1f9a3ef162
Dimitri <dnikitop@gmail.com>
2020-01-03 09:32:25 -0500

    fix: comments from PR

--- scripts/figure_eight.py
+++ scripts/figure_eight.py
@@ -10,22 +10,22 @@ import tf
 from srslib_framework.msg import MsgUpdateToteLights
 
 
-def messageCreation(message, cmd, sC, eC, sS, eS, freq):
+def messageCreation(message, cmd, startColor, endColor, startSegment, endSegment, freq):
     message.lightCmd = cmd
-    message.startColor.r = sC[0]
-    message.startColor.g = sC[1]
-    message.startColor.b = sC[2]
-    message.startColor.a = sC[3]
-    message.endColor.r = eC[0]
-    message.endColor.g = eC[1]
-    message.endColor.b = eC[2]
-    message.endColor.a = eC[3]
-    message.startSegment.x = sS[0]
-    message.startSegment.y = sS[1]
-    message.startSegment.z = sS[2]
-    message.endSegment.x = eS[0]
-    message.endSegment.y = eS[1]
-    message.endSegment.z = eS[2]
+    message.startColor.r = startColor[0]
+    message.startColor.g = startColor[1]
+    message.startColor.b = startColor[2]
+    message.startColor.a = startColor[3]
+    message.endColor.r = endColor[0]
+    message.endColor.g = endColor[1]
+    message.endColor.b = endColor[2]
+    message.endColor.a = endColor[3]
+    message.startSegment.x = startSegment[0]
+    message.startSegment.y = startSegment[1]
+    message.startSegment.z = startSegment[2]
+    message.endSegment.x = endSegment[0]
+    message.endSegment.y = endSegment[1]
+    message.endSegment.z = endSegment[2]
     message.frequency = freq
 
 
@@ -35,11 +35,11 @@ class FigureEight:
 	LIGHT_TOPIC = "/drivers/brainstem/cmd/update_tote_lights"
 	global rospy
 
-	def __init__(self,offX, offY, rad):
-		self.offsetX = offX
-		self.offsetY = offY
-		self.radius = rad
-		self.vel_sub = rospy.Subscriber(self.VEL_COM_TOPIC, Twist, self.velCb)
+	def __init__(self, offsetX, offsetY, radius):
+		self.offsetX = offsetX
+		self.offsetY = offsetY
+		self.radius = radius
+		self.vel_sub = rospy.Subscriber(self.VEL_COM_TOPIC, Twist, self.velocityCmdCallback)
 		self.goal_pub = rospy.Publisher(self.GOAL_TOPIC, Path, queue_size=2)
 		self.light_pub = rospy.Publisher(self.LIGHT_TOPIC, MsgUpdateToteLights, queue_size=5)
 		self.sendGoal = True
@@ -58,10 +58,10 @@ class FigureEight:
 				newPose = PoseStamped()
 				newPose.header.seq = i
 				newPose.header.frame_id = 'map'
-				newPose.pose.position.x = math.cos(i*3.14159/50.0) * self.radius + self.offsetX
-				newPose.pose.position.y = math.sin(i*3.14159/50.0) * self.radius + self.offsetY
+				newPose.pose.position.x = math.cos(i*math.pi/50.0) * self.radius + self.offsetX
+				newPose.pose.position.y = math.sin(i*math.pi/50.0) * self.radius + self.offsetY
 				newPose.pose.position.z = 0
-				newQuaternion = tf.transformations.quaternion_from_euler(0, 0, i*3.14159/50.0 + 3.14159/2)
+				newQuaternion = tf.transformations.quaternion_from_euler(0, 0, i*math.pi/50.0 + math.pi/2)
 				newPose.pose.orientation.x = 0
 				newPose.pose.orientation.y = 0
 				newPose.pose.orientation.z = newQuaternion[2]
@@ -72,10 +72,10 @@ class FigureEight:
 				newPose = PoseStamped()
 				newPose.header.seq = i
 				newPose.header.frame_id = 'map'
-				newPose.pose.position.x = -1 * math.cos(i*3.14159/50.0) * self.radius + self.offsetX + 2 * self.radius
-				newPose.pose.position.y = math.sin(i*3.14159/50.0) * self.radius + self.offsetY
+				newPose.pose.position.x = -1 * math.cos(i*math.pi/50.0) * self.radius + self.offsetX + 2 * self.radius
+				newPose.pose.position.y = math.sin(i*math.pi/50.0) * self.radius + self.offsetY
 				newPose.pose.position.z = 0
-				newQuaternion = tf.transformations.quaternion_from_euler(0, 0, -1 * i*3.14159/50.0 + 3.14159/2)
+				newQuaternion = tf.transformations.quaternion_from_euler(0, 0, -1 * i*math.pi/50.0 + math.pi/2)
 				newPose.pose.orientation.x = 0
 				newPose.pose.orientation.y = 0
 				newPose.pose.orientation.z = newQuaternion[2]
@@ -85,7 +85,7 @@ class FigureEight:
 		print "woop"
 		self.sendGoal = False
 
-	def velCb(self, msg):
+	def velocityCmdCallback(self, msg):
 		if(msg.linear.x == 0 and msg.angular.z == 0):
 			self.sendGoal = True
 			self.firstLoop =  not self.firstLoop
